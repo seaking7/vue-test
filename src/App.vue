f45ee3e39cea39d448d3ac223c010cbed8e3d876
김태경 <taekyung@lguplus.co.kr>
2023-05-20 11:29:35 +0900

    project init2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 @click="increase"> count : {{count}}</h1>
+  <div v-if="count > 4"> 4보다 큽니다!</div>
+  <ul>
+    <Fruit 
+    v-for="fruit in fruits" 
+    :key="fruit"
+    :name="fruit">
+    {{ fruit }}
+  </Fruit>
+  </ul>
+  <HelloWorld msg="Welcome to Taekyung"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Fruit from './components/FruitData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Fruit
+  },
+  data(){
+    return {
+      count: 2,
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  beforeCreate(){
+    console.log('Before Create', this.count)
+  },
+  created(){
+    console.log('Created!', this.count)
+  },
+  beforeMount() {
+    console.log('Before mount')
+  },
+  mounted(){
+    console.log('Mounted')
+    console.log(document.querySelector('h1'))
+  },
+  methods: {
+    increase(){
+      this.count += 1
+    }
   }
 }
 </script>
 
 <style>
+h1{
+  font-size: 50px;
+  color: royalblue;
+}
+ul > li {
+  font-size: 30px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/FruitData.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <li>{{ name }}?!</li>
+</template>
+  
+<script>
+  export default {
+    props: {
+        name: {
+            type: String,
+            default: ''
+        }
+    }
+  }
+</script>
+  
+<style scoped>
+  li {
+    color: red;
+  }
+</style>
+  
